962e436fbb8053c61d35e99f16fa937424b3d0c1
Brandon Garcia <grandonleebarcia@gmail.com>
2024-04-30 12:45:54 -0700

    added site header

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: 'BrandonBlog',
+  url: 'https://example.com',
+  description: 'NextJS 14 blog using velite, tailwind, and shadcn',
+  links: {
+    twitter: '',
+    github: '',
+    personalSite: '',
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
